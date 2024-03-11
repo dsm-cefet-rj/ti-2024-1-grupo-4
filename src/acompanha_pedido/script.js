7136e2bfdf5072608c842b00c8d9e8e87e07f083
Darkowns <propaltalt@gmail.com>
2024-03-11 00:42:19 -0300

    add-../acompanhapedido

---
+++ src/acompanha_pedido/script.js
@@ -0,0 +1,12 @@
+function DetalhesLista() {
+    document.getElementById("PedidoRealizado").innerHTML = "&nbsp  Pedido Realizado";
+    document.getElementById("TextoDefault").innerHTML = "&nbsp&nbsp&nbsp  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod";
+    document.getElementById("PagamentoConfirmado").innerHTML = "&nbsp  Pagamento Confirmado";
+    document.getElementById("Preparo").innerHTML = "&nbsp  Preparo";
+    document.getElementById("Saiu").innerHTML = "&nbsp  Saiu para Entrega";
+    document.getElementById("Previsão").innerHTML = "&nbsp  Previsão de Entrega/Pedido Finalizado";
+    document.getElementById("TextoDefault2").innerHTML = "&nbsp&nbsp&nbsp  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod";
+    document.getElementById("TextoDefault3").innerHTML = "&nbsp&nbsp&nbsp  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod";
+    document.getElementById("TextoDefault4").innerHTML = "&nbsp&nbsp&nbsp  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod";
+    document.getElementById("TextoDefault5").innerHTML = "&nbsp&nbsp&nbsp  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod";
+}
