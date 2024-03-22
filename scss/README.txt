cd331af7aa19cac39e20d3fca66f62c19c01db5f
Darkowns <propaltalt@gmail.com>
2024-03-22 18:27:38 -0300

    Criação da base do novo projeto

---
+++ scss/README.txt
@@ -0,0 +1,1 @@
+sass --watch ./scss/custom.scss ./css/custom.css
---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './componentes/header/Header'
+import Home from './componentes/home/Home'
+import Footer from './componentes/footer/Footer'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <Header/>
+        <Home/>
+        <Footer/>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
