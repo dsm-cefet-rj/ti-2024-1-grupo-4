c94b38b8aba11c6605ce09b8729e265e3658cd75
Emanuelle <emanuellegmnsa@gmail.com>
2024-03-30 18:05:40 -0300

    esqueleto-autenticacao-local

--- src/componentes/Login/Login.jsx
+++ src/componentes/Login/Login.jsx
@@ -1,12 +1,27 @@
-import './login.css';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import './login.scss';
+import { Form, Button} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import users from './users';
+//import { verifyUser } from './auth';
 
-function Login_page() {
+
+const Login_page = () => {
+  const [username, setUsername] = useState('');
+  const[password, setPassword] = useState('');
+  const[error, setError] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    if(users.find(users.username === username && users.password === password)){
+      return true;
+    } else{
+      return false;
+    }
+  }; 
 
   return (
-    <div className="wrapper">
+    <div className="wrapper container-fluid">
       <div className = "classe-login">
         <div className = "icone-svg">
           <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="currentColor" className="bi bi-person-fill" viewBox="0 0 16 16">
@@ -18,15 +33,18 @@ function Login_page() {
           <Form>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>E-mail</Form.Label>
-              <Form.Control className = "classe-input" type="email" placeholder="E-mail" />
+              <Form.Control className = "classe-input" type="email" placeholder="E-mail" value = {username}  onChange={e => setUsername(e.target.value)}/>
+              { error && <Form.Control.Feedback type = "invalid">Login ou Senha errados</Form.Control.Feedback> }
             </Form.Group>
+            
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
               <Form.Label>Senha</Form.Label>
-              <Form.Control className = "classe-input" type="password" placeholder="Senha" />
+              <Form.Control className = "classe-input" type="password" placeholder="Senha" value={password} onChange={e => setPassword(e.target.value)}/>
             </Form.Group>
+            
             <div className = "botao-login">
-              <Button className = "classe-input custom-button bg-equator" type="submit">
+              <Button className = "classe-input custom-button bg-equator" type="submit" onClick={handleLogin}>
                 Login
             </Button> 
             </div>
---
+++ src/componentes/Login/users.js
@@ -0,0 +1,10 @@
+let users = [
+    {
+      "id": 1,
+      "username": "emanu@manu",
+      "password": "nunu"
+    }
+  ]
+
+
+export default users;
