fc757a408353149928afecb2702f10587f31ceff
HernaniFilho <hernanipcf@gmail.com>
2024-03-30 12:43:32 -0300

    Nada relevante

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import Header from './componentes/header/Header.jsx';
 import Home from './componentes/home/Home.jsx';
 import About from './componentes/about/About.jsx';
 import Footer from './componentes/footer/Footer.jsx';
-import '../scss/custom.scss';
 import Form from './componentes/forms/Forms'
 
 function App(){
