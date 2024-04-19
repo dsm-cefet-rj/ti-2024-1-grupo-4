155f09fa2ceb9a42b8b0c746881ad92a9358931c
HernaniFilho <hernanipcf@gmail.com>
2024-04-19 13:49:29 -0300

    Mudança importante no schema de Produto

--- src/componentes/admin/ProdutoSchema.js
+++ src/componentes/admin/ProdutoSchema.js
@@ -4,6 +4,6 @@ export const productSchema = Yup.object().shape({
     id: Yup.string(),
     nome: Yup.string().required().max(50),
     imgUrl: Yup.string().required(),
-    preco: Yup.number().required().max(30),
+    preco: Yup.number().required(),
     descricao: Yup.string().required().max(500),
 });
