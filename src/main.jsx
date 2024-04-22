db77ff862e558f58541c302e3a91377051b2290e
VicerTi <vicerv2010@gmail.com>
2024-04-21 22:14:02 -0300

    Message

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import Login from './pages/Login.jsx'
 import Registro from './pages/Cadastro.jsx'
 import Pedido from './pages/Pedido.jsx'
 import Admin_Page from './pages/Admin_Page.jsx'
+
 import HistoricoPedido from './pages/HistoricoPedido.jsx'
 
 
@@ -43,7 +44,7 @@ const router = createBrowserRouter([
     element:<Admin_Page/>,
   },
   {
-    path: "Historico",
+    path: "historico",
     element: <HistoricoPedido/>,
   }
 ]);
