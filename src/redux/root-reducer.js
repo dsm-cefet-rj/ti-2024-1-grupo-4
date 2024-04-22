e12d7cedff9b0b361c752510e963ab834cf0fa3d
VicerTi <vicerv2010@gmail.com>
2024-04-22 02:18:41 -0300

    Merge branch 'PSWHistoricoCommitAlternativo' of https://github.com/dsm-cefet-rj/ti-2024-1-grupo-4 into PSWHistoricoCommitAlternativo

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,8 +2,9 @@ import { combineReducers } from "redux"
 import userSlice from './user/UserSlice';
 import cartSlicer from './cart/CartSlicer';
 import produtosSlice from './produtos/ProdutosSlice';
+import pedidoSlice from './listapedidos/ListaPedidoSlice';
 import enderecoSlice from "./endereco/enderecoSlice";
 
-const rootReducer = combineReducers({ userSlice, cartSlicer , produtosSlice, enderecoSlice});
+const rootReducer = combineReducers({ userSlice, cartSlicer , produtosSlice, enderecoSlice, pedidoSlice});
 
 export default rootReducer;
