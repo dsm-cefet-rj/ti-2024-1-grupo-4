56c040caf7b850e5e214a3442807dd168772fb31
HernaniFilho <hernanipcf@gmail.com>
2024-04-23 19:05:49 -0300

    Listando Pedido do cliente

--- src/componentes/Lista/setuplista_function.jsx
+++ src/componentes/Lista/setuplista_function.jsx
@@ -12,7 +12,6 @@ import { useDispatch, useSelector } from 'react-redux';
 
 function Lista(ped) {
   const dispatch = useDispatch();
-  const status = useSelector((rootReducer) => rootReducer.pedidoSlice.status);
 
   const handleDeletePedido = () => {
     dispatch(deletePedidoServer(ped.id));
@@ -28,8 +27,28 @@ function Lista(ped) {
         </div>
       </div>
       <div class="collapse" id={ped.id}>
-        <div class="card-body align-items-center">
-            <span>Teste</span>
+        <div class="card-body">
+          <div className='d-flex flex-column'>
+            <h5>Endereço para entrega (CEP): <span>{ped.endereco.cep}</span></h5>
+            <span>Local: {ped.endereco.logradouro}, {ped.endereco.numEnd}, {ped.endereco.bairro}</span>
+            <span>Complemento: {ped.endereco.CompEnd}</span>
+            <span>Instruções: {ped.endereco.instrucao_pedido}</span>
+          </div>
+          <h5 className='border-bottom border-tacao'>Produtos:</h5>
+            {
+              ped.products.map((produto) => (
+                <>
+                  <div className='d-flex justify-content-between align-items-center'>
+                    <div className='d-flex justify-content-between align-items-center gap-3'>
+                      <span>{produto.nome}</span>
+                      <span>({produto.preco.toLocaleString('pt-br',{style: 'currency', currency:'BRL'})})</span>
+                    </div>
+                    <span>x{produto.quantity}</span>
+                  </div>
+                </>
+              ))
+            }
+          <h5 className='border-top border-tacao'>Total: {ped.valorTotal.toLocaleString('pt-br',{style: 'currency', currency:'BRL'})}</h5>
         </div>
       </div>
     </>
