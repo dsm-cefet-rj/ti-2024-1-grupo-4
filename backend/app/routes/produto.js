f395052f4959d542c7418916db34ae6bf9b1e513
darkownss <jeanflp1@gmail.com>
2024-05-17 21:29:02 -0300

    Crud produto parcial feito

--- backend/app/routes/produto.js
+++ backend/app/routes/produto.js
@@ -93,16 +93,34 @@ router.route('/:id')
   }
   )
   .put((req, res, next) => {
-    produto = produto.filter(
-      (value, index, arr) => {
-        return value.id != req.params.id;
-      }
-    );
+    // .map(produto=>produto.id) = arrow function de um objeto produto de produto(o array e produtos),
+    // onde retorna para o map o id desse objeto, lendo-se:
+    //dentro de um array de ids de produtos, há um produto igual ao id do parametro da requisition -> poem em index
+
+    let index = produto.map(produto => produto.id).indexOf(req.params.id);
+    //slice = Removes elements from an array and, if necessary, inserts new elements in their place, returning the deleted elements.
+    /**
+     *     splice(start: number, deleteCount?: number): T[];
+    /**
+     * Removes elements from an array and, if necessary, inserts new elements in their place, returning the deleted elements.
+     * @param start The zero-based location in the array from which to start removing elements.
+     * @param deleteCount The number of elements to remove.
+     * @param items Elements to insert into the array in place of the deleted elements.
+     * @returns An array containing the elements that were deleted.
+     */
+    produto.splice(index,1,req.body);
 
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
-    res.json(req.params.id);
+    //retorna body segundo o splice e produto
+    res.json(req.body);
   }
   )
 
 module.exports = router;
+
+/**
+ * ° criar as exceptions dentro do backend
+ * 
+ * 
+ */
