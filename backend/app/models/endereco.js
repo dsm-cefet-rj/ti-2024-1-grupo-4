fccd4485525e2b6881355194085f4b82d06b6050
Emanuelle <emanuellegmnsa@gmail.com>
2024-05-25 15:34:00 -0300

    Endereco_Schema

--- backend/app/models/endereco.js
+++ backend/app/models/endereco.js
@@ -1,8 +1,32 @@
 const mongoose = require('mongoose');
-const user = require('./users');
 
 
 const EnderecoSchema = mongoose.Schema(
-
+    {
+        id:{
+            type: String,
+            required: true,
+        },
+        CEP:{
+            type: String,
+            required: true,
+        },
+        logradouro:{
+            type: String,
+            required: true,
+        },
+        numero:{
+            type: Number,
+            required: true,
+        },
+        complemento:{
+            type: String,
+            required: false,
+        },
+        userKey:{
+            type: String,
+            required: true,
+        }
+    }
 );
 module.exports = mongoose.model('endereco',EnderecoSchema);
