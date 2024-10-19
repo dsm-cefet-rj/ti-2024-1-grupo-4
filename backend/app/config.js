137d8f1ddfdcc38063ac0d9a43a921985d061278
Jean F. <65645756+darkownss@users.noreply.github.com>
2024-10-19 12:33:54 -0300

    Update config.js

--- backend/app/config.js
+++ backend/app/config.js
@@ -1,9 +1,4 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb+srv://propaltalt2:asd@cluster-luigipizzeria.v7sy7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-luigipizzeria'
+    'mongoUrl': '##########################'
 }
-
-/**
- * mongodb://localhost:27017/luigipizzeria
- * mongodb+srv://propaltalt2:asd@cluster-luigipizzeria.v7sy7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster-luigipizzeria
- */
